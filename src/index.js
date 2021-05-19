923cffc541d283c9a1b84f4b526ba4e4aa30c6ae
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2021-05-19 16:19:07 +0200

    router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+
 import Connexion from './components/Connexion'
+import NotFound from "./components/NotFound";
+import App from "./App";
+
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Connexion />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+const Root = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={Connexion}/>
+            <Route path='/pseudo/:pseudo' component={App}/>
+            <Route component={NotFound} />
+        </Switch>
+    </BrowserRouter>
+)
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
