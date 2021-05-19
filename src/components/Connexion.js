ff8b1220d3275db60c4ee77722840a106839b8aa
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2021-05-19 17:08:24 +0200

    changement de page

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,15 +1,37 @@
 import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom'
 import '../App.css';
 
 class Connexion extends Component {
+    state = {
+        pseudo: '',
+        goToChat: false
+    }
+
+    handleChange = event => {
+        const pseudo = event.target.value
+        this.setState({pseudo})
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.setState({ goToChat:true })
+    }
+
     render() {
+        if (this.state.goToChat) {
+            return <Redirect to={`/pseudo/${this.state.pseudo}`}></Redirect>
+        }
+
         return (
             <div className='connexionBox'>
-                <form className='connexion'>
+                <form className='connexion' onSubmit={this.handleSubmit}>
                     <input
-                    placeholder='pseudo'
-                    type="text"
-                    required
+                        value={this.state.pseudo}
+                        onChange={this.handleChange}
+                        placeholder='pseudo'
+                        type="text"
+                        required
                     />
                     <button type='submit'>GO</button>
                 </form>
