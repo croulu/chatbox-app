470478fed8cc9d369a2f43a4e791b0f75de995c1
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2021-05-20 16:47:36 +0200

    ajout des composannts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import {Component} from "react";
 import './App.css';
+import Formulaire from "./components/Formulaire";
+import Message from "./components/Message";
 
 class App extends Component {
     render() {
         return (
             <div className="box">
-yo
+                <div className="messages">
+                    <Message></Message>
+                    <Message></Message>
+                    <Message></Message>
+                </div>
+                <Formulaire />
             </div>
         )
     }
