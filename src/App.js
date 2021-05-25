80d892e6b646cfe3c2d5cfcd17cf9ec8133a1f4d
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2021-05-25 17:35:30 +0200

    enregistrer les messages dans le state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import Formulaire from "./components/Formulaire";
 import Message from "./components/Message";
 
 class App extends Component {
+    state = {
+        messages: {},
+        pseudo: this.props.match.params.pseudo
+    }
+
+    addMessage = message => {
+        const messages = {...this.state.messages}
+        messages[`message-${Date.now()}`] = message
+        this.setState({messages})
+    }
+
     render() {
         return (
             <div className="box">
@@ -12,7 +23,9 @@ class App extends Component {
                     <Message></Message>
                     <Message></Message>
                 </div>
-                <Formulaire />
+                <Formulaire
+                    pseudo={this.state.pseudo}
+                    addMessage={this.addMessage}/>
             </div>
         )
     }
